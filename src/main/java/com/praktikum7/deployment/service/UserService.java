f53b31ee19040685d48a84508fb3c939ec864c6a
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-04-20 14:31:21 +0700

    Menambahkan User untuk getUserById

--- src/main/java/com/praktikum7/deployment/service/UserService.java
+++ src/main/java/com/praktikum7/deployment/service/UserService.java
@@ -25,4 +25,8 @@ public class UserService {
     public List<User> getAllUser(){
         return userRepository.findAll();
     }
+
+    public User getUserById(String id){
+        return UserRepository.findById(id).orElse(null)
+    }
 }
