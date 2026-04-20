c6f35c509c49ae6a1db85dad6b726171b19e1f7a
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-04-20 14:38:33 +0700

    menambahkan createUser

--- src/main/java/com/praktikum7/deployment/controller/UserController.java
+++ src/main/java/com/praktikum7/deployment/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.praktikum7.deployment.controller;
 
 
+import com.praktikum7.deployment.model.User;
+import com.praktikum7.deployment.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 
 public class UserController {
+    private final UserService userService;
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody User request){
+        userService.addUser(request);
+        return "user created successfully";
+    }
 }
