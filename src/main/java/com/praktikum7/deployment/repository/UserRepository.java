93a30a9754484919363b57b785c96541c1d05b4b
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-04-20 14:21:18 +0700

    Menambahkan UserRepository untuk JpaRepository

--- src/main/java/com/praktikum7/deployment/repository/UserRepository.java
+++ src/main/java/com/praktikum7/deployment/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.praktikum7.deployment.repository;
 
 
+import com.praktikum7.deployment.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, String> {
+
 }
