e28ea1c4a2f51f7090828cf51edfab45eab683dc
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-04-20 14:23:05 +0700

    Menambahkan userRepository dan parameter constructor

--- src/main/java/com/praktikum7/deployment/service/UserService.java
+++ src/main/java/com/praktikum7/deployment/service/UserService.java
@@ -1,10 +1,17 @@
 package com.praktikum7.deployment.service;
 
 
+import com.praktikum7.deployment.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
 
 }
