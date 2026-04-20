99a37545edc042e8f7e8bd124a611d937ca05519
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-04-20 14:12:38 +0700

    menambahkan @Entity, @Data, @Table, @AllArgs, dan @NoArgs

---
+++ src/main/java/com/praktikum7/deployment/model/User.java
@@ -0,0 +1,17 @@
+package com.praktikum7.deployment.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table (name="20230140126_users")
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+}
